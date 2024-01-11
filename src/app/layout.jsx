ac10326568a773faa64662efc547ab1805e9ada6
Mauricio Orellana <mauricioeorellana@gmail.com>
2024-01-11 11:51:20 +0100

    footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "./components/Navbar"
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>{children}
       <Navbar />
       
-      
+      <Footer />
       
       </body>
     </html>
