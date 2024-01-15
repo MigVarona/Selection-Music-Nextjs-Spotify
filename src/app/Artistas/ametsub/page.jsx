55509a00a45cedf92d318bf048645cf9b8b97433
PaolaGuerra <paola.gueho@gmail.com>
2024-01-15 14:48:02 +0100

    artistas

--- src/app/Artistas/ametsub/page.jsx
+++ src/app/Artistas/ametsub/page.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-export default function Moderat() {
+
+export default function Ametsub() {
   return (
     <section className="bg-white ">
       <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
@@ -12,18 +13,18 @@ export default function Moderat() {
               <Image 
                 className="object-cover object-center w-full h-full mx-auto rounded-lg shadow-2xl"
                 width={900}
-                height={600} alt="hero" src="/MODERAT_.jpg" />
+                height={600} alt="hero" src="/Artistas/Amtsub.jpg" />
               </div>
             </div>
           </div>
           <div className="flex flex-col items-center mt-12 lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0 ">
-            <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl text-center lg:text-left">MODERAT</h1>
-            <p className="mb-8 text-base leading-relaxed text-gray-500 text-center lg:text-left">Moderat es un innovador proyecto musical formado por Sascha Ring (Apparat) y Gernot Bronsert y Sebastian Szary (Modeselektor).</p>
+            <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl text-center lg:text-left">AMETSUB</h1>
+            <p className="mb-8 text-base leading-relaxed text-gray-500 text-center lg:text-left">Ametsub es el nombre artístico del músico japonés Yu Miyashita, reconocido por sus contribuciones a la música electrónica ambiental y experimental.</p>
             <div className="mt-0 lg:mt-6 w-full max-w-3xl mx-auto sm:flex justify-center">
               <div className="mt-3 rounded-lg sm:mt-0">
                 <iframe
                   style={{ borderRadius: '12px' }}
-                  src="https://open.spotify.com/embed/playlist/4OTkdKh9y2bnGfq5cIVsgz?utm_source=generator&theme=0"
+                  src="https://open.spotify.com/embed/artist/1lqeOtFoEHyRGR9QoRJpEz?utm_source=generator&theme=0"
                   width="100%"
                   height="152"
                   frameBorder="0"
@@ -36,6 +37,5 @@ export default function Moderat() {
           </div>
         </div>
       </div>
-    </section>
-  );
+    </section>  );
 }
