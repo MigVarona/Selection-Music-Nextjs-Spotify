7157c5e51dcc83a7081ab5b4bd6a1cb48ce97c51
MigVarona <migvaronag@gmail.com>
2024-01-15 14:49:49 +0100

    ok

--- src/app/components/Albums.jsx
+++ src/app/components/Albums.jsx
@@ -22,7 +22,7 @@ const Album = () => {
   }, []);
 
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12 mt-60">
+    <div className="bg-white py-6 sm:py-8 lg:py-12 mt-40">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         {/* Sección de texto */}
         <div className="mb-10 md:mb-16">
